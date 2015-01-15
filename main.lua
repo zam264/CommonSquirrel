2a1e2747cc9a015f47939989263039de1e65900d
zam264@gmail.com <Zodiac2648!>
2015-01-14 21:08:46 -0500

    Added visuals and game mechanics
    Added a scrolling background which appears to endlessly scroll.  Added
    images to be used for background.  Randomized 'enemy' speed after enemy
    goes off screen and returns.  Slightly increased square size.  Added
    variable which holds random speed modifier.  Added constants for
    'enemies' for easy stat manipulation.  Dropped some excessive code.

--- main.lua
+++ main.lua
@@ -1,17 +1,34 @@
+--display.setStatusBar( display.HiddenStatusBar )
+
+display.setDefault( "background", 80/255 )
+local bg = display.newImageRect( "bg1.jpg", 1080, 1920 )
+bg.x = display.contentCenterX
+bg.y = display.contentHeight
+local bg1 = display.newImageRect( "bg1.jpg", 1080, 1920 )
+bg1.x = display.contentCenterX
+bg1.y = 0
+
+
 local function newRect(params)
 	local rect = display.newRect(params.xpos, params.ypos, params.width, params.height)
 	rect:setFillColor(params.r, params.g, params.b, 255)
 	rect.xdir = params.xdir
 	rect.ydir = params.ydir
+	rect.speed = params.speed
 	
 	return rect
 end
 
+
+local w = 100
+local h = 100
+local s = 1
+local y = 10
 local params = {
-	{xpos=display.contentWidth*0.25, ypos=display.contentHeight*.25, width=50, height=50, xdir=1, ydir=1, r=255, g=0, b=0},
-	{xpos=display.contentWidth*0.5, ypos=display.contentHeight*.5, width=50, height=50, xdir=1, ydir=1, r=255, g=0, b=0},
-	{xpos=display.contentWidth*0.75, ypos=display.contentHeight*.75, width=50, height=50, xdir=1, ydir=1, r=255, g=0, b=0},
-	{xpos=0, ypos=1100, width=50, height=50, xdir=0, ydir=0, r=0, g=255, b=0}
+	{xpos=display.contentWidth*0.25, ypos=display.contentHeight*.25, width=w, height=h, ydir=y, r=255, g=0, b=0, speed=s},
+	{xpos=display.contentWidth*0.5, ypos=display.contentHeight*.5, width=w, height=h, ydir=y, r=255, g=0, b=0, speed=s},
+	{xpos=display.contentWidth*0.75, ypos=display.contentHeight*.75, width=w, height=h, ydir=y, r=255, g=0, b=0, speed=s},
+	{xpos=0, ypos=display.contentHeight*0.85, width=w, height=h, ydir=0, r=0, g=255, b=0, speed=0}
 }
 
 local collection = {}
@@ -27,27 +44,34 @@ local screenLeft = display.screenOriginX
 local screenRight = display.viewableContentWidth + display.screenOriginX
 
 function collection:enterFrame(event)
+	if(bg.y-960 > display.contentHeight)then 
+		bg.y = -960
+	elseif(bg1.y-960 > display.contentHeight)then	
+		bg1.y = -960
+	else
+		bg:translate(0, 3)
+		bg1:translate(0, 3)
+	end
+
 	for _,rect in ipairs(collection) do
-		local dx = (0*rect.xdir)
-		local dy = (10*rect.ydir)
-		local xNew, yNew = rect.x + dx, rect.y + dy
+		local dy = (rect.ydir*rect.speed)
+		local yNew = rect.y + dy
 		local player = table.maxn(collection)
-		
 		local width, height = rect.width, rect.height
-		if(yNew > screenBottom - height/2 or yNew < screenTop + height/2) then
-			--rect.ydir = -rect.ydir
-			rect.y = 25
-		elseif(rect.x == collection[player].x and rect.y+50 > collection[player].y and rect.y < collection[player].y+50 and rect ~= collection[player])then
+
+		if(yNew > screenBottom - height/2 ) then
+			rect.y = -50
+			rect.speed = math.random( 1, 10 )/10+1
+		elseif(rect.x == collection[player].x and rect.y+50 > collection[player].y-50 and rect.y-50 < collection[player].y+50 and rect ~= collection[player])then
 			collection[player].x = 0
 		end
-		
-		rect:translate(dx, dy)
+
+		rect:translate(0, dy)
 	end
 end
 
 local function printTouch(event)
 	print("event: " .. event.phase .. "\n x: " .. event.x .. "\n y: " .. event.y)
-	print("array size:" .. table.maxn(collection))
 	local player = table.maxn(collection)
 	if(event.phase == "began")then
 		if(event.x > display.contentWidth*0.5 and collection[player].x <= display.contentWidth*0.5)then
