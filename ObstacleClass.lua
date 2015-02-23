25a75c2b427dc4a6d9d5d242decbc13ff2467530
Steven Zamborsky <zam264@gmail.com>
2015-02-23 13:50:55 -0500

    Skeletons are no more
    I've replaced the skeletons with bee hives that have a buzzing
    animation.

--- ObstacleClass.lua
+++ ObstacleClass.lua
@@ -11,28 +11,21 @@ function Obstacle:init(posX, posY)
 	spriteOptions = {	
 		height = 64, 
 		width = 64, 
-		numFrames = 273, 
-		sheetContentWidth = 832, 
-		sheetContentHeight = 1344 
+		numFrames = 3, 
+		sheetContentWidth = 192, 
+		sheetContentHeight = 64 
 	}
 
-	mySheet = graphics.newImageSheet("skeleton_3.png", spriteOptions, 2000, 2000)
+	mySheet = graphics.newImageSheet("imgs/beeSheet.png", spriteOptions)--, 2000, 2000)
 	sequenceData = {
-		{name = "forward", frames={105,106,107,108,109,110,111,112}, time = 500, loopCount = 1},
-		{name = "right", frames={144,145,146,147,148,149,150,151,152}, time = 500, loopCount = 1}, 
-		{name = "back", frames= {131,132,133,134,135,136,137,138,139}, time = 500, loopCount = 1}, 
-		{name = "left", frames={118,119,120,121,122,123,124,125,126}, time = 500, loopCount = 1},
-		{name = "attackForward", frames={157,158,159,160,161,162}, time = 700, loopCount = 0},
-		{name = "attackRight", frames={196,197,198,199,200,201}, time = 700, loopCount = 0},
-		{name = "attackBack", frames={183,184,185,186,187,188}, time = 700, loopCount = 0},
-		{name = "attackLeft", frames={170,171,172,173,174,175}, time = 700, loopCount = 0},
-		{name = "death", frames={261,262,263,264,265,266}, time = 500, loopCount = 1}
+		{name = "buzz", frames={1, 2, 3, 2, 1}, time = 150, loopCount = 0},
 	}	
 	
 	-- Display the new sprite at the coordinates passed
 	self.model = display.newSprite(mySheet, sequenceData)
 	self.model.type = "obstacle"  --Define the type of obstacle for collision detection
-	self.model:setSequence("forward")
+	self.model:setSequence("buzz")
+	self.model:play()
 	self.model.x = posX
 	self.model.y = posY
 	self.model.xScale = display.contentWidth * .0025
