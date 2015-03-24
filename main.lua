50630008a3a1848b9d824679004d5208329a0204
zgp1001 <zacharypetrusch@gmail.com>
2015-03-23 21:35:31 -0400

    Added back button code
    Commented out currently due to a small bug

--- main.lua
+++ main.lua
@@ -7,7 +7,6 @@
 -- hide the status bar
 display.setStatusBar( display.HiddenStatusBar )
 
--- include the Corona "storyboard" module
 local composer = require "composer"
 
 -- load menu screen
@@ -15,21 +14,33 @@ local rect
 holding = {}
 inUse = {}
 
- 
 composer.gotoScene( "menu" )
 
 --Codes the keys for back button functionality 
 local function onKeyEvent(event)
 	local phase = event.phase
     local keyName = event.keyName 
-    
-	if ( "back" == keyName and phase == "up" ) then
-		--composer.gotoScene( storyboard.returnTo, "fade", 250 )
+--[[
+    if ( "back" == keyName and phase == "up" ) then
+		if ( composer.getCurrentSceneName() == "menu" ) then
+        	native.requestExit()
+        elseif ( composer.getCurrentSceneName() == "pause" ) then
+        	composer.removeScene( "pause" )
+			composer.gotoScene( "game", {effect="fromLeft", time=1000}) 
+		elseif ( composer.getCurrentSceneName() == "achievements" or 
+				 composer.getCurrentSceneName() == "options" 	  or 
+				 composer.getCurrentSceneName() == "unlockables") 	then
+			composer.gotoScene("menu", {effect="fromRight", time=1000})
+		elseif ( composer.getCurrentSceneName() == "gamecredits" or composer.getCurrentSceneName() == "journal") then
+			composer.gotoScene("options", {effect="fromRight", time=1000})
+        end
 		return true	-- indicates successful touch
 		
 	else 
 		return false ; 
 	end
+	]]
+ 
 end
 
 Runtime:addEventListener( "key", onKeyEvent )
